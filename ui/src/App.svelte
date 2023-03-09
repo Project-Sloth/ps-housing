3ac213385809bed7be3fdd812bb37734b33c626d
OK1ez <87883296+OK1ez@users.noreply.github.com>
2023-03-09 23:04:35 +0100

    base menu

--- ui/src/App.svelte
+++ ui/src/App.svelte
@@ -3,12 +3,13 @@
 	import { browserMode, resName } from '@store/stores'
 	import DebugBrowser from '@providers/DebugBrowser.svelte'
 	import AlwaysListener from '@providers/AlwaysListener.svelte'
+	import Menu from '@components/Menu.svelte'
 
 	$resName = 'ps-housing' // Change this to your resource name (case sensitive)
 </script>
 
 <VisibilityProvider>
-	<!-- PUT STUFF HERE  -->
+	<Menu />
 </VisibilityProvider>
 
 
