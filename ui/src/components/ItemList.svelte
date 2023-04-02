9ef69549d97bd2b19dc553f7a369e20cabcecbf3
Xirvin <xander228irvin@gmail.com>
2023-04-02 21:48:15 +1000

    Item list

--- ui/src/components/ItemList.svelte
+++ ui/src/components/ItemList.svelte
@@ -19,7 +19,17 @@
 		class="grid grid-rows-2 grid-flow-col-dense gap-[1vw] overflow-x-scroll w-fit max-w-full h-full relative scroll-style"
 	>
 		{#each { length: 100 } as _, i}
-			<div class="h-full w-[15vw] bg-[color:var(--color-secondary)]" />
+			<div class="h-full w-[15vw] bg-[color:var(--color-secondary)] flex flex-row">
+                <!-- Furniture name -->
+                <div class="w-full h-full flex flex-col justify-center items-start p-[1vw]">
+                    <p class="text-[color:var(--color-text)] text-[1vw] font-bold">Furniture Name</p>
+                    <p class="text-[color:var(--color-text)] text-[1vw]">Price</p>
+                </div>
+                <!-- eye button -->
+                <button class=" bg-[color:var(--color-tertiary)] aspect-square h-full grid place-items-center justify-center items-center">
+                    <i class="fas fa-eye text-[color:var(--color-text)] text-[1.5vw]"></i>
+                </button>
+            </div>  
 		{/each}
 	</div>
 </div>
