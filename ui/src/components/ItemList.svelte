d0a51c5fc44c4400c2f2227bef7265ab4d513cb1
Xirvin <xander228irvin@gmail.com>
2023-06-18 16:55:58 +1000

    add modal for switching furniture whiole one is selected

--- ui/src/components/ItemList.svelte
+++ ui/src/components/ItemList.svelte
@@ -1,13 +1,19 @@
 <script lang="ts">
 	import { IS_MENU_MINIMIZED, SHOWFURNITURES, CURRENTFURNITURE } from '@store/stores'
 	import { SendNUI } from '@utils/SendNUI'
+	import Portal from './Portal.svelte'
+
 	let scrollableElement: HTMLElement
+
 	function scrollHorizontally(e) {
 		e = window.event || e
 		var delta = Math.max(-1, Math.min(1, e.wheelDelta || -e.detail))
 		scrollableElement.scrollLeft -= delta * 50 // Multiplied by 40
 		e.preventDefault()
 	}
+
+	let showModal: boolean = false
+	let switchToFurniture: any = null
 </script>
 
 <div  class="w-full h-[24rem] bg-[color:var(--color-primary)] p-[1rem]">
@@ -20,9 +26,17 @@
 			<button
 				class="h-full w-[30rem] bg-[color:var(--color-secondary)] flex flex-row items-center px-[1.5rem] py-[0.5rem] hover:cursor-pointer"
 				on:click={() => {
-					SendNUI("previewFurniture", furniture)
-					$CURRENTFURNITURE = furniture
-					$IS_MENU_MINIMIZED = true
+					if ($CURRENTFURNITURE) {
+						showModal = true
+						switchToFurniture = furniture
+					} else {
+						SendNUI("previewFurniture", furniture)
+						$CURRENTFURNITURE = furniture
+						$IS_MENU_MINIMIZED = true
+					}
+				}}
+				on:mouseleave={() => {
+					SendNUI("hoverOut")
 				}}
 			>
 				<!-- Furniture name -->
@@ -57,6 +71,40 @@
 	</div>
 </div>
 
-<style>
+{#if showModal}
+<Portal>
+	<div class="z-[100] bg-black bg-opacity-50 w-screen h-screen absolute top-0 left-0 grid place-items-center">
+		<div class="bg-[color:var(--color-secondary)] absolute w-[50rem] h-[fit] p-[1vw] flex flex-col gap-[1vw] justify-center items-center">
+			<p class="text-[color:var(--color-text)] text-[2rem] font-bold text-start">
+				Are you sure you want to stop placing this current furniture?
+			</p>
+			<div class="flex flex-row justify-between w-full gap-[2vw]">
+				<button
+					class="bg-[color:var(--color-tertiary)] text-[color:var(--color-text)] text-[2rem] px-[1rem] py-[0.5rem] w-full hover:cursor-pointer"
+					on:click={() => {
+						showModal = false
+						switchToFurniture = null
+					}}
+				>
+					No
+				</button>
+				<button
+				class="bg-[color:var(--color-tertiary)] text-[color:var(--color-text)] text-[2rem] px-[1rem] py-[0.5rem] w-full hover:cursor-pointer"
+				on:click={() => {
+					SendNUI("previewFurniture", switchToFurniture)
+					$CURRENTFURNITURE = switchToFurniture
+					$IS_MENU_MINIMIZED = true
 
-</style>
+					showModal = false
+					switchToFurniture = null
+				}}
+			>
+				Yes
+			</button>
+			</div>
+		</div>
+	</div>
+
+</Portal>
+
+{/if}
---
+++ ui/src/components/Portal.svelte
@@ -0,0 +1,11 @@
+<script>
+	function portal(node, { to }) {
+		const target = document.querySelector(to)
+		target && target.appendChild(node)
+		return {}
+	}
+</script>
+
+<div use:portal={{ to: 'body' }} class="absolute">
+    <slot />
+</div>
