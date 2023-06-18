1660be794c29bc9f79c0ce0063e11394263560e9
Xirvin <xander228irvin@gmail.com>
2023-06-18 19:27:32 +1000

    Update config.lua

--- shared/config.lua
+++ shared/config.lua
@@ -453,7 +453,7 @@ Config.Furnitures = {
         category = "Prerequisites",
         items = {
             { ["object"] = "v_res_tre_storagebox", ["price"] = 0, ["label"] = "Storage Unit", ["type"] = "storage" },
-            { ["object"] = "tr_int1_clotheslocker", ["price"] = 0, ["label"] = "Outfit Mirror", ["type"] = "clothing"}
+            { ["object"] = "v_26_wardrobe", ["price"] = 0, ["label"] = "Wardrobe", ["type"] = "clothing"}
         }
     },
 
