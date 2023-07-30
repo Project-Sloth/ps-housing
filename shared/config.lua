9f08bf9875420b90b9fdc94eac89f10f8a718848
LeSiiN <103898231+LeSiiN@users.noreply.github.com>
2023-07-30 22:08:01 +0200

    Update config.lua

--- shared/config.lua
+++ shared/config.lua
@@ -11,6 +11,7 @@ Config.Target = "ox" -- "ox" or "qb"
 Config.Notify = "ox" -- "ox" or "qb"
 Config.Radial = "ox" -- "ox" or "qb"
 Config.Inventory = "ox" -- "ox" or "qb"
+Config.Doorlock = "ox" -- "ox" or "qb"
 
 -- Anyone provided with keys to a property has the ability to modify its furnishings.
 Config.AccessCanEditFurniture = true
