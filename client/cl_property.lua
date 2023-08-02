be5e38edcb840a8f82dc21b2fb7a938ffa1ec7d1
JnKTechstuff <7122547+JnKTechstuff@users.noreply.github.com>
2023-08-02 06:32:02 -0400

    Update cl_property.lua
    Do not use Player because it is a preset global

--- client/cl_property.lua
+++ client/cl_property.lua
@@ -32,8 +32,7 @@ function Property:new(propertyData)
     propertyData.furnitures = {}
     self.propertyData = propertyData
 
-    local Player = QBCore.Functions.GetPlayerData()
-    local citizenid = Player.citizenid
+    local citizenid = PlayerData.citizenid
 
     self.owner = propertyData.owner == citizenid
     self.has_access = lib.table.contains(self.propertyData.has_access, citizenid)
@@ -630,8 +629,7 @@ end
 function Property:UpdateOwner(newOwner)
     self.propertyData.owner = newOwner
 
-    local Player = QBCore.Functions.GetPlayerData()
-    local citizenid = Player.citizenid
+    local citizenid = PlayerData.citizenid
 
     self.owner = newOwner == citizenid
 
@@ -660,8 +658,7 @@ function Property:UpdateDoor(newDoor, newStreet, newRegion)
 end
 
 function Property:UpdateHas_access(newHas_access)
-    local Player = QBCore.Functions.GetPlayerData()
-    local citizenid = Player.citizenid
+    local citizenid = PlayerData.citizenid
     self.propertyData.has_access = newHas_access
     self.has_access = lib.table.contains(newHas_access, citizenid)
 
