1c41b2ca0b61628bae3a74b070fed7a9cc29102c
ItsMuri <88394932+ItzMuri@users.noreply.github.com>
2024-07-07 21:17:37 +0200

    Update image links config.lua (#220)

--- shared/config.lua
+++ shared/config.lua
@@ -146,7 +146,7 @@ Config.Shells = {
         },
         imgs = {
             {
-                url = "https://cdn.discordapp.com/attachments/1101313033684394084/1101712181017460736/motel.webp",
+                url = "https://files.fivemerr.com/images/eaf77207-da0e-4bd1-be03-324ba0e2ffdc.webp",
                 label = "Motel",
             },
         }
@@ -162,11 +162,11 @@ Config.Shells = {
         },
         imgs = {
             {
-                url = "https://cdn.discordapp.com/attachments/1101313033684394084/1101712459691208704/angle_1.webp",
+                url = "https://files.fivemerr.com/images/e21b7997-c86e-435a-9140-a319c65e5ad8.webp",
                 label = "Angle 1",
             },
             {
-                url = "https://cdn.discordapp.com/attachments/1101313033684394084/1101712460110643210/angle_2.webp",
+                url = "https://files.fivemerr.com/images/27a59fc9-2739-4a0d-a1a2-020a5e849389.webp",
                 label = "Angle 2",
             },
         }
@@ -182,27 +182,27 @@ Config.Shells = {
         },
         imgs = {
             {
-                url = "https://cdn.discordapp.com/attachments/1101313033684394084/1101712598019354684/bathroom.webp",
+                url = "https://files.fivemerr.com/images/36e5d763-a00f-44a7-a114-82d054073320.webp",
                 label = "Bathroom",
             },
             {
-                url = "https://cdn.discordapp.com/attachments/1101313033684394084/1101712598409420850/Bedroom.webp",
+                url = "https://files.fivemerr.com/images/263234eb-479e-4b6e-9aeb-6640c25f7672.webp",
                 label = "Bedroom",
             },
             {
-                url = "https://cdn.discordapp.com/attachments/1101313033684394084/1101712598807887942/entry.webp",
+                url = "https://files.fivemerr.com/images/eacbe324-d2d6-4312-8fae-15eff63ad1fc.webp",
                 label = "Entrance",
             },
             {
-                url = "https://cdn.discordapp.com/attachments/1101313033684394084/1101712599185371146/Kitchen_and_Dining.webp",
+                url = "https://files.fivemerr.com/images/62051872-d44d-4bd0-ba0f-f8a4607a9e3d.webp",
                 label = "Kitchen and Dining",
             },
             {
-                url = "https://cdn.discordapp.com/attachments/1101313033684394084/1101712599629963366/living_room_1.webp",
+                url = "https://files.fivemerr.com/images/4ffd2c5c-78ad-454e-bc58-e66cd94c5b35.webp",
                 label = "Living Room Angle 1",
             },
             {
-                url = "https://cdn.discordapp.com/attachments/1101313033684394084/1101712599982293022/living_room_2.webp",
+                url = "https://files.fivemerr.com/images/1a628bcc-ded2-4902-97f2-b6e6862d2d65.webp",
                 label = "Living Room Angle 2",
             },
         },
@@ -218,23 +218,23 @@ Config.Shells = {
         },
         imgs = {
             {
-                url = "https://cdn.discordapp.com/attachments/1101313033684394084/1101712919412088955/bathroom.webp",
+                url = "https://files.fivemerr.com/images/90238f10-e65e-4c89-a879-6be664194258.webp",
                 label = "Bathroom",
             },
             {
-                url = "https://cdn.discordapp.com/attachments/1101313033684394084/1101712919735054417/bedroom.webp",
+                url = "https://files.fivemerr.com/images/edfcda70-bca6-419c-84b5-11e173e9edc3.webp",
                 label = "Bedroom",
             },
             {
-                url = "https://cdn.discordapp.com/attachments/1101313033684394084/1101712920083189770/entry.webp",
+                url = "https://files.fivemerr.com/images/aed9c846-7314-4869-948f-3262e1d141ee.webp",
                 label = "Entrance",
             },
             {
-                url = "https://cdn.discordapp.com/attachments/1101313033684394084/1101712920464859257/kitchen.webp",
+                url = "https://files.fivemerr.com/images/a51a4844-2141-472f-bde2-0c07a940ddd0.webp",
                 label = "Kitchen",
             },
             {
-                url = "https://cdn.discordapp.com/attachments/1101313033684394084/1101712920846544907/livingroom.webp",
+                url = "https://files.fivemerr.com/images/bcbc0b02-5648-4eb2-bd16-5585a2f14e12.webp",
                 label = "Living Room",
             },
         },
@@ -250,15 +250,15 @@ Config.Shells = {
         },
         imgs = {
             {
-                url = "https://cdn.discordapp.com/attachments/1101313033684394084/1101713104288612384/entry.webp",
+                url = "https://files.fivemerr.com/images/febe4d12-7432-4da6-ad6a-1957c74f8024.webp",
                 label = "Entrance",
             },
             {
-                url = "https://cdn.discordapp.com/attachments/1101313033684394084/1101713104926154853/kitchen.webp",
+                url = "https://files.fivemerr.com/images/a74b3377-81ca-4df3-9bd0-e4f0828f78bf.webp",
                 label = "Kitchen",
             },
             {
-                url = "https://cdn.discordapp.com/attachments/1101313033684394084/1101713105333006357/livingroom.webp",
+                url = "https://files.fivemerr.com/images/66eb8b85-e0c7-4065-b230-32410772260a.webp",
                 label = "Living Room",
             },
         },
@@ -274,11 +274,11 @@ Config.Shells = {
         },
         imgs = {
             {
-                url = "https://cdn.discordapp.com/attachments/1101313033684394084/1101713200220745748/entry.webp",
+                url = "https://files.fivemerr.com/images/6c6fda4c-1f03-4ccb-b9e0-22ba358e6821.webp",
                 label = "Entrance",
             },
             {
-                url = "https://cdn.discordapp.com/attachments/1101313033684394084/1101713200677933126/garage.webp",
+                url = "https://files.fivemerr.com/images/c5aecca6-7831-497a-ad3e-dcf06f7db4f6.webp",
                 label = "Garage",
             },
         },
@@ -294,11 +294,11 @@ Config.Shells = {
         },
         imgs = {
             {
-                url = "https://cdn.discordapp.com/attachments/1101313033684394084/1101713285117648976/entry.webp",
+                url = "https://files.fivemerr.com/images/d8987d5a-af22-4927-aae0-3c69f75038d3.webp",
                 label = "Entance",
             },
             {
-                url = "https://cdn.discordapp.com/attachments/1101313033684394084/1101713285449007196/office.webp",
+                url = "https://files.fivemerr.com/images/b1f584b8-0570-4400-b868-30cd2d7fd17f.webp",
                 label = "Office",
             },
         },
@@ -314,11 +314,11 @@ Config.Shells = {
         },
         imgs = {
             {
-                url = "https://cdn.discordapp.com/attachments/1101313033684394084/1101713353291878420/room_1.webp",
+                url = "https://files.fivemerr.com/images/454ae047-3948-41aa-bea2-e276ed99aec6.webp",
                 label = "Room 1",
             },
             {
-                url = "https://cdn.discordapp.com/attachments/1101313033684394084/1101713353937789018/room_2.webp",
+                url = "https://files.fivemerr.com/images/bbd41423-2782-4597-9320-d45590abe327.webp",
                 label = "Room 2",
             },
         },
@@ -334,11 +334,11 @@ Config.Shells = {
         },
         imgs = {
             {
-                url = "https://cdn.discordapp.com/attachments/1101313033684394084/1101713451149172766/angle1.webp",
+                url = "https://files.fivemerr.com/images/4f39ffc7-6de4-435a-a1a3-b0e9493c43fd.webp",
                 label = "Angle 1",
             },
             {
-                url = "https://cdn.discordapp.com/attachments/1101313033684394084/1101713451551838278/angle2.webp",
+                url = "https://files.fivemerr.com/images/a6c7e4ed-ff81-4b30-adca-89d51f5ee64e.webp",
                 label = "Angle 2",
             },
         },
@@ -354,7 +354,7 @@ Config.Shells = {
         },
         imgs = {
             {
-                url = "https://cdn.discordapp.com/attachments/1101313033684394084/1101713526319497267/container.webp",
+                url = "https://files.fivemerr.com/images/6c7cdceb-7106-4559-97e6-bcc58e24ce9b.webp",
                 label = "Container",
             },
         },
@@ -370,39 +370,39 @@ Config.Shells = {
         },
         imgs = {
             {
-                url = "https://cdn.discordapp.com/attachments/1101313033684394084/1101713596683145267/bathroom.webp",
+                url = "https://files.fivemerr.com/images/b9813f35-ce5d-4ebd-a4b2-d538628dcd20.webp",
                 label = "Bathroom",
             },
             {
-                url = "https://cdn.discordapp.com/attachments/1101313033684394084/1101713597232586777/entry.webp",
+                url = "https://files.fivemerr.com/images/eadb895d-9186-4b08-b50d-6c4e08fef952.webp",
                 label = "Entrance",
             },
             {
-                url = "https://cdn.discordapp.com/attachments/1101313033684394084/1101713597698166844/floor2.webp",
+                url = "https://files.fivemerr.com/images/5320e158-010e-4ac3-9e93-3fe08b150486.webp",
                 label = "Floor 2",
             },
             {
-                url = "https://cdn.discordapp.com/attachments/1101313033684394084/1101713598201471106/kitchen.webp",
+                url = "https://files.fivemerr.com/images/95a36c06-a00a-4d84-aa3d-439f51e99d8f.webp",
                 label = "Kitchen",
             },
             {
-                url = "https://cdn.discordapp.com/attachments/1101313033684394084/1101713598734159962/livingroom.webp",
+                url = "https://files.fivemerr.com/images/74f9a597-1643-48e3-a215-b347a01a74de.webp",
                 label = "Living Room",
             },
             {
-                url = "https://cdn.discordapp.com/attachments/1101313033684394084/1101713599178752020/room1.webp",
+                url = "https://files.fivemerr.com/images/572734b6-e68c-46c1-9693-86e20e393a10.webp",
                 label = "Room 1",
             },
             {
-                url = "https://cdn.discordapp.com/attachments/1101313033684394084/1101713599690448956/room2.webp",
+                url = "https://files.fivemerr.com/images/a64ddbfe-c374-465c-89c1-363588ab25e0.webp",
                 label = "Room 2",
             },
             {
-                url = "https://cdn.discordapp.com/attachments/1101313033684394084/1101713600176996394/room3.webp",
+                url = "https://files.fivemerr.com/images/ec212d2b-107c-4da7-aea2-d61139cc6bef.webp",
                 label = "Room 3",
             },
             {
-                url = "https://cdn.discordapp.com/attachments/1101313033684394084/1101713596179816499/room3dresser.webp",
+                url = "https://files.fivemerr.com/images/fdd207a7-ec15-44b2-9dde-347aaa7cfd67.webp",
                 label = "Room 3 Dresser",
             },
         },
@@ -418,31 +418,31 @@ Config.Shells = {
         },
         imgs = {
             {
-                url = "https://cdn.discordapp.com/attachments/1101313033684394084/1101713982059974656/bathroom.webp",
+                url = "https://files.fivemerr.com/images/d0557708-bb57-4dea-b8c7-8ec34c8391a4.webp",
                 label = "Bathroom",
             },
             {
-                url = "https://cdn.discordapp.com/attachments/1101313033684394084/1101713982491983942/diningroom.webp",
+                url = "https://files.fivemerr.com/images/81b849bb-c722-42f3-865b-a5c20da6d1be.webp",
                 label = "Dining Room",
             },
             {
-                url = "https://cdn.discordapp.com/attachments/1101313033684394084/1101713982848512030/entry.webp",
+                url = "https://files.fivemerr.com/images/c795a770-fb95-4838-9d12-182ebfefc681.webp",
                 label = "Entrance",
             },
             {
-                url = "https://cdn.discordapp.com/attachments/1101313033684394084/1101713983175659530/kitchen.webp",
+                url = "https://files.fivemerr.com/images/a095ee16-c074-41a3-9c6a-d56fb8ce3a47.webp",
                 label = "Kitchen",
             },
             {
-                url = "https://cdn.discordapp.com/attachments/1101313033684394084/1101713983490228285/livingroom.webp",
+                url = "https://files.fivemerr.com/images/ad22df64-4d64-4fde-b0ba-5dcaa0659d97.webp",
                 label = "Living Room",
             },
             {
-                url = "https://cdn.discordapp.com/attachments/1101313033684394084/1101713983821590578/room1.webp",
+                url = "https://files.fivemerr.com/images/fc96a826-048a-4dda-a825-2fa43b8f7179.webp",
                 label = "Room 1",
             },
             {
-                url = "https://cdn.discordapp.com/attachments/1101313033684394084/1101713984169709690/room2.webp",
+                url = "https://files.fivemerr.com/images/e7292195-e518-49e7-9ba8-284b8f2dcc30.webp",
                 label = "Room 2",
             },
         },
@@ -458,23 +458,23 @@ Config.Shells = {
         },
         imgs = {
             {
-                url = "https://cdn.discordapp.com/attachments/1101313033684394084/1101714310809518131/entry.webp",
+                url = "https://files.fivemerr.com/images/eb936202-0bf2-468b-809a-625ccba14698.webp",
                 label = "Entrance",
             },
             {
-                url = "https://cdn.discordapp.com/attachments/1101313033684394084/1101714311228964956/hallway.webp",
+                url = "https://files.fivemerr.com/images/9e8ac33b-d71d-4ed9-81cc-e1b407d86e76.webp",
                 label = "Hallway",
             },
             {
-                url = "https://cdn.discordapp.com/attachments/1101313033684394084/1101714311623213177/kitchen.webp",
+                url = "https://files.fivemerr.com/images/b5fe556f-efa7-4048-a0f3-8e604d885fb4.webp",
                 label = "Kitchen",
             },
             {
-                url = "https://cdn.discordapp.com/attachments/1101313033684394084/1101714311988133910/mainarea.webp",
+                url = "https://files.fivemerr.com/images/e9728ca8-24c5-4915-9329-3fccb59450dd.webp",
                 label = "Main Area",
             },
             {
-                url = "https://cdn.discordapp.com/attachments/1101313033684394084/1101714312394977370/room.webp",
+                url = "https://files.fivemerr.com/images/31fbd065-6789-4106-a667-7a73c557486b.webp",
                 label = "Room",
             },
         },
@@ -490,15 +490,15 @@ Config.Shells = {
         },
         imgs = {
             {
-                url = "https://i.imgur.com/ddg9zHU.jpeg",
+                url = "https://files.fivemerr.com/images/d9501f26-81d1-46d6-81cd-56a0345a3c1b.jpeg",
                 label = "Angle 1",
             },
             {
-                url = "https://i.imgur.com/4rvkeme.jpeg",
+                url = "https://files.fivemerr.com/images/480bc73e-977e-417a-b70b-78fc337dec69.jpeg",
                 label = "Angle 2",
             },
             {
-                url = "https://i.imgur.com/4QAcZBp.jpeg",
+                url = "https://files.fivemerr.com/images/e8bec3f0-0d87-4ccd-9ff1-6b6a1141c097.jpeg",
                 label = "Entrance",
             },
         },
@@ -514,27 +514,27 @@ Config.Shells = {
         },
         imgs = {
             {
-                url = "https://cdn.discordapp.com/attachments/1101313033684394084/1101714761596543107/bathroom.webp",
+                url = "https://files.fivemerr.com/images/e709bb71-7d05-476c-81d8-c42ef53025f5.webp",
                 label = "Bathroom",
             },
             {
-                url = "https://cdn.discordapp.com/attachments/1101313033684394084/1101714761994997760/entry.webp",
+                url = "https://files.fivemerr.com/images/516ec833-2c14-49a2-8f5d-13c6d3cd2651.webp",
                 label = "Entrance",
             },
             {
-                url = "https://cdn.discordapp.com/attachments/1101313033684394084/1101714762343133227/kitchen.webp",
+                url = "https://files.fivemerr.com/images/186a0e06-4e57-4bd6-9ff7-c05ae2557638.webp",
                 label = "Kitchen",
             },
             {
-                url = "https://cdn.discordapp.com/attachments/1101313033684394084/1101714762741588018/livingroom.webp",
+                url = "https://files.fivemerr.com/images/5c1461f0-bcef-4f23-828b-a78ffca86c0c.webp",
                 label = "Living Room",
             },
             {
-                url = "https://cdn.discordapp.com/attachments/1101313033684394084/1101714763140042772/room1.webp",
+                url = "https://files.fivemerr.com/images/a74b278c-ece9-4287-9d60-6b7deb752463.webp",
                 label = "Room 1",
             },
             {
-                url = "https://cdn.discordapp.com/attachments/1101313033684394084/1101714763500761158/room2.webp",
+                url = "https://files.fivemerr.com/images/3e2c7ae3-3166-47a7-9991-9ab749f7ff29.webp",
                 label = "Room 2",
             },
         },
@@ -550,15 +550,15 @@ Config.Shells = {
         },
         imgs = {
             {
-                url = "https://cdn.discordapp.com/attachments/1101313033684394084/1101714470985809920/entranceandkitchen.webp",
+                url = "https://files.fivemerr.com/images/9c55b4d8-d529-4d7e-b3c3-9c6abe8e3cca.webp",
                 label = "Entrance and Kitchen",
             },
             {
-                url = "https://cdn.discordapp.com/attachments/1101313033684394084/1101714471329747026/main.webp",
+                url = "https://files.fivemerr.com/images/4677d95d-d6b1-4577-ab8a-6d08f6401445.webp",
                 label = "Main",
             },
             {
-                url = "https://cdn.discordapp.com/attachments/1101313033684394084/1101714471665274980/room1.webp",
+                url = "https://files.fivemerr.com/images/5c0eac55-d394-49b2-b2de-e9d31d9bb7c8.webp",
                 label = "Room",
             },
         },
